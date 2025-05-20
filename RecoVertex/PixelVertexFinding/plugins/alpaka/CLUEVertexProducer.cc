1b6af6a0dc29221b411e62ca9179158cf325db67
Jonayed <jonayed.ahmed@studio.unibo.it>
2025-05-20 12:16:02 +0200

    Added Vertex positions and Cluster ids to 'vertices' object

--- RecoVertex/PixelVertexFinding/plugins/alpaka/CLUEVertexProducer.cc
+++ RecoVertex/PixelVertexFinding/plugins/alpaka/CLUEVertexProducer.cc
@@ -31,125 +31,78 @@
 namespace ALPAKA_ACCELERATOR_NAMESPACE {
 
   class CLUEVertexProducer : public global::EDProducer<> {
-   using TkSoADevice = TracksSoACollection<pixelTopology::Phase1>;
-   public: 
+    using TkSoADevice = TracksSoACollection<pixelTopology::Phase1>;
+
+  public:
     CLUEVertexProducer(edm::ParameterSet const& conf)
-	: EDProducer(conf), 
-      verbose_(conf.getParameter<int>("Verbosity")),
-      // 1.0 GeV
-      ptMin_(conf.getParameter<double>("PtMin")),
-      method2(conf.getParameter<bool>("Method2")),
-      trackCollName(conf.getParameter<edm::InputTag>("TrackCollection")),
-      token_Tracks(consumes(trackCollName)),
-      //token_BeamSpot(consumes(conf.getParameter<edm::InputTag>("beamSpot"))),
-      token_RecoVertex(produces("CLUEVertex")) {
-  // Register my product
+        : EDProducer(conf),
+          verbose_(conf.getParameter<int>("Verbosity")),
+          // 1.0 GeV
+          ptMin_(conf.getParameter<double>("PtMin")),
+          method2(conf.getParameter<bool>("Method2")),
+          trackCollName(conf.getParameter<edm::InputTag>("TrackCollection")),
+          token_Tracks(consumes(trackCollName)),
+          //token_BeamSpot(consumes(conf.getParameter<edm::InputTag>("beamSpot"))),
+          token_RecoVertex(produces("CLUEVertex")) {
+      // Register my product
 
-  // Setup shop
-  // std::string finder = conf.getParameter<std::string>("Finder");  // DivisiveVertexFinder
-  bool useError = conf.getParameter<bool>("UseError");            // true
-  bool wtAverage = conf.getParameter<bool>("WtAverage");          // true
-  double zOffset = conf.getParameter<double>("ZOffset");          // 5.0 sigma
-  double zSeparation = conf.getParameter<double>("ZSeparation");  // 0.05 cm
-  int ntrkMin = conf.getParameter<int>("NTrkMin");                // 3
-  // Tracking requirements before sending a track to be considered for vtx
+      // Setup shop
+      // std::string finder = conf.getParameter<std::string>("Finder");  // DivisiveVertexFinder
+      bool useError = conf.getParameter<bool>("UseError");            // true
+      bool wtAverage = conf.getParameter<bool>("WtAverage");          // true
+      double zOffset = conf.getParameter<double>("ZOffset");          // 5.0 sigma
+      double zSeparation = conf.getParameter<double>("ZSeparation");  // 0.05 cm
+      int ntrkMin = conf.getParameter<int>("NTrkMin");                // 3
+      // Tracking requirements before sending a track to be considered for vtx
 
-  double track_pt_min = ptMin_;
-  double track_pt_max = 10.;
-  double track_chi2_max = 9999999.;
-  double track_prob_min = -1.;
-  std::cout << track_pt_min << " " << track_pt_max << " " << track_chi2_max << " " << track_prob_min << "\n";
-  if (conf.exists("PVcomparer")) {
-    edm::ParameterSet PVcomparerPSet = conf.getParameter<edm::ParameterSet>("PVcomparer");
-    track_pt_min = PVcomparerPSet.getParameter<double>("track_pt_min");
-    track_pt_max = PVcomparerPSet.getParameter<double>("track_pt_max");
-    track_chi2_max = PVcomparerPSet.getParameter<double>("track_chi2_max");
-    track_prob_min = PVcomparerPSet.getParameter<double>("track_prob_min");
-  }
+      double track_pt_min = ptMin_;
+      double track_pt_max = 10.;
+      double track_chi2_max = 9999999.;
+      double track_prob_min = -1.;
+      std::cout << track_pt_min << " " << track_pt_max << " " << track_chi2_max << " " << track_prob_min << "\n";
+      if (conf.exists("PVcomparer")) {
+        edm::ParameterSet PVcomparerPSet = conf.getParameter<edm::ParameterSet>("PVcomparer");
+        track_pt_min = PVcomparerPSet.getParameter<double>("track_pt_min");
+        track_pt_max = PVcomparerPSet.getParameter<double>("track_pt_max");
+        track_chi2_max = PVcomparerPSet.getParameter<double>("track_chi2_max");
+        track_prob_min = PVcomparerPSet.getParameter<double>("track_prob_min");
+      }
+    }
+    ~CLUEVertexProducer() override = default;
+    static void fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+      edm::ParameterSetDescription desc;
+      desc.add<int>("Verbosity", 0);
+      desc.add<double>("PtMin", 1.0);
+      desc.add<bool>("Method2", true);
+      desc.add<edm::InputTag>("TrackCollection", edm::InputTag("pixelTracks"));
+      desc.add<edm::InputTag>("beamSpot", edm::InputTag("offlineBeamSpot"));
+      desc.add<std::string>("Finder", "DivisiveVertexFinder");
+      desc.add<bool>("UseError", true);
+      desc.add<bool>("WtAverage", true);
+      desc.add<double>("ZOffset", 5.0);
+      desc.add<double>("ZSeparation", 0.05);
+      desc.add<int>("NTrkMin", 2);
+      {
+        edm::ParameterSetDescription PVComparerPSet;
 
-  }
-   ~CLUEVertexProducer() override = default;
-   static void fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+        PVComparerPSet.add<double>("track_pt_min", 1.0);
 
+        PVComparerPSet.add<double>("track_pt_max", 10.0);
 
-  edm::ParameterSetDescription desc;
+        PVComparerPSet.add<double>("track_chi2_max", 999999.);
 
+        PVComparerPSet.add<double>("track_prob_min", -1.);
 
-  desc.add<int>("Verbosity", 0);
+        desc.addOptional<edm::ParameterSetDescription>("PVcomparer", PVComparerPSet);
+      }
 
+      // check label
 
-  desc.add<double>("PtMin", 1.0);
+      descriptions.addWithDefaultLabel(desc);
 
-
-  desc.add<bool>("Method2", true);
-
-
-  desc.add<edm::InputTag>("TrackCollection", edm::InputTag("pixelTracks"));
-
-
-  desc.add<edm::InputTag>("beamSpot", edm::InputTag("offlineBeamSpot"));
-
-
-  desc.add<std::string>("Finder", "DivisiveVertexFinder");
-
-
-  desc.add<bool>("UseError", true);
-
-
-  desc.add<bool>("WtAverage", true);
-
-
-  desc.add<double>("ZOffset", 5.0);
-
-
-  desc.add<double>("ZSeparation", 0.05);
-
-
-  desc.add<int>("NTrkMin", 2);
-
-
-
-
-
-  {
-
-
-    edm::ParameterSetDescription PVComparerPSet;
-
-
-    PVComparerPSet.add<double>("track_pt_min", 1.0);
-
-
-    PVComparerPSet.add<double>("track_pt_max", 10.0);
-
-
-    PVComparerPSet.add<double>("track_chi2_max", 999999.);
-
-
-    PVComparerPSet.add<double>("track_prob_min", -1.);
-
-
-    desc.addOptional<edm::ParameterSetDescription>("PVcomparer", PVComparerPSet);
-
-
-  }
-
-
-
-
-
-  // check label
-
-
-  descriptions.addWithDefaultLabel(desc);
-
-
-  //descriptions.add("CLUEVertex", desc);
-
-
-}
+      //descriptions.add("CLUEVertex", desc);
+    }
     void produce(edm::StreamID sid, device::Event& event, device::EventSetup const&) const override {
-	
       auto const& tracks_d = event.get(token_Tracks);
       /* It's layout is:
              SOA_COLUMN(Quality, quality),
@@ -166,7 +119,7 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE {
       */
       // const auto& bsHandle = event.get(token_BeamSpot);
       std::cout << "Pippo \n";
-      
+
       auto const& tracks_d_view = tracks_d.view();
       Queue queue = event.queue();
       int maxVertices = 10;
@@ -175,11 +128,11 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE {
 
       ZVertexSoACollection vertices({{maxVertices, maxTracks}}, queue);
       auto data = vertices.view();
-      auto trkdata = vertices.view<reco::ZVertexTracksSoA>(); // access the data in the ZVertexTracksSoA Layout
-      auto vrtxdata = vertices.view<reco::ZVertexSoA>(); // access the data in the ZVertexSoA Layout
+      auto trkdata = vertices.view<reco::ZVertexTracksSoA>();  // access the data in the ZVertexTracksSoA Layout
+      auto vrtxdata = vertices.view<reco::ZVertexSoA>();       // access the data in the ZVertexSoA Layout
       // Copying from device to host
       TracksHost<pixelTopology::Phase1> tracks_h(queue);
-      alpaka::memcpy(queue, tracks_h.buffer(), tracks_d.buffer()); 
+      alpaka::memcpy(queue, tracks_h.buffer(), tracks_d.buffer());
 
       std::vector<float> coords;
       std::vector<int> results(nTracks);
@@ -192,8 +145,9 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE {
 
       auto my_clusters = std::span<const int>{results.data(), nTracks};
       auto isSeed = std::span<const int>(results.data() + nTracks, nTracks);
-        
-      std::vector<int> clusterCount(maxVertices); // need this to calculate averages later
+
+      int nClusters = *(std::max_element(my_clusters.begin(), my_clusters.end())) + 1;
+      std::vector<int> clusterCount(nClusters);  // need this to calculate averages later
       /* ZVertexSoACollection is made of a ZVertexSoA and a ZVertexTracksSoA
       // ZvertexSoA is made of:
       //               SOA_COLUMN(float, zv),          // output z-posistion of found vertices
@@ -206,7 +160,7 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE {
          SOA_COLUMN(int16_t, idv),   // vertex index for each associated (original) track
                                      // (-1 == not associate)
          SOA_COLUMN(int32_t, ndof))  // vertices number of dof
-      */  
+      */
 
       // Let's start filling out "vertices" !!
       // To fill out columns and scalars I have to use memcopys, cause I am on the host
@@ -215,13 +169,20 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE {
         zv_hbuff[i] = coords[i];
       }*/
 
-      std::for_each(my_clusters.begin(), my_clusters.end(), [] (int &idx) { clusterCount[idx]++; };
+      // I will need this to compute chi2 of each vertex
+      std::for_each(my_clusters.begin(), my_clusters.end(), [&clusterCount](int idx) { clusterCount[idx]++; });
+      vrtxdata.nvFinal() = nClusters;
+      float chi2 = 0;
+
+      std::vector<float> zAcc(nClusters);
 
       for (auto i = 0u; i < nTracks; ++i) {
-        if(isSeed[i]) {
-            data[i].zv() = coords[i];
+        if (isSeed[i]) {
+          vrtxdata[my_clusters[i]].zv() = coords[i];
+          trkdata[i].ndof() = clusterCount[my_clusters[i]] - 1;
         }
-        clusterCount[my_clusters[i]]++;
+        trkdata[i].idv() = my_clusters[i];
+        zAcc[my_clusters[i]] += coords[i];
       }
 
       event.emplace(token_RecoVertex, std::move(vertices));
@@ -231,11 +192,11 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE {
       auto vertexes = std::make_unique<reco::VertexCollection>();
       auto my_clusters = algo.getClusters(h_points); // returns std::map<int, std::vector<int>> vertex to track ids map
       auto seeds = h_points.isSeed(); // array of indexes of the seeds, there are my_clusters.size() seeds
-	*/	
+	*/
       // Need to put the clusters into "vertexes", use the seed of each cluster as the point, and then put the points of the
       // cluster in the track of each Vector
       //
-     /* if (bsHandle.isValid()) {
+      /* if (bsHandle.isValid()) {
         
 	const reco::BeamSpot& bs = *bsHandle;
 
@@ -276,27 +237,27 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE {
 	  int ndof = clusterSize - 1;
       }*/
 
-      // event.put(std::move(vertexes));      
-    } 
-  private:
+      // event.put(std::move(vertexes));
+    }
 
-	// ----------member data ---------------------------
-  // Turn on debug printing if verbose_ > 0
-  const int verbose_;
-  // Tracking cuts before sending tracks to vertex algo
-  const double ptMin_;
-  const bool method2;
-  const edm::InputTag trackCollName;
-  device::EDGetToken<TkSoADevice> token_Tracks;
-  // const device::EDGetToken<reco::BeamSpot> token_BeamSpot;
-  device::EDPutToken<ZVertexSoACollection> token_RecoVertex;
+  private:
+    // ----------member data ---------------------------
+    // Turn on debug printing if verbose_ > 0
+    const int verbose_;
+    // Tracking cuts before sending tracks to vertex algo
+    const double ptMin_;
+    const bool method2;
+    const edm::InputTag trackCollName;
+    device::EDGetToken<TkSoADevice> token_Tracks;
+    // const device::EDGetToken<reco::BeamSpot> token_BeamSpot;
+    device::EDPutToken<ZVertexSoACollection> token_RecoVertex;
     // Parameters for CLUEAlgoAlpaka
-    float m_dc{1.5f}; // Side length of box to calculate density
-    float m_rhoc{10.f}; // Minimum energy density to NOT be an outlier 
-    float m_dm{1.5f}; // Side length of box to search for followers
-    int m_pPBin{128}; // Average number of points found in a tile
-    bool m_wtAvg{true}; // Decides how to copute error
+    float m_dc{1.5f};    // Side length of box to calculate density
+    float m_rhoc{10.f};  // Minimum energy density to NOT be an outlier
+    float m_dm{1.5f};    // Side length of box to search for followers
+    int m_pPBin{128};    // Average number of points found in a tile
+    bool m_wtAvg{true};  // Decides how to copute error
   };
-}
+}  // namespace ALPAKA_ACCELERATOR_NAMESPACE
 
 DEFINE_FWK_ALPAKA_MODULE(CLUEVertexProducer);
