a4968d47d95b129356f9acff1e0995e52f2750da
Simone <simone.balducci00@gmail.com>
2025-08-12 18:29:48 +0200

    Update CLUE vertex producer to latest changes in DataFormats

--- RecoVertex/PixelVertexFinding/plugins/alpaka/CLUEVertexProducer.cc
+++ RecoVertex/PixelVertexFinding/plugins/alpaka/CLUEVertexProducer.cc
@@ -31,7 +31,7 @@
 namespace ALPAKA_ACCELERATOR_NAMESPACE {
 
   class CLUEVertexProducer : public global::EDProducer<> {
-    using TkSoADevice = TracksSoACollection<pixelTopology::Phase2>;
+    using TkSoADevice = reco::TracksSoACollection;
 
   public:
     CLUEVertexProducer(edm::ParameterSet const& conf)
@@ -120,35 +120,28 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE {
       // const auto& bsHandle = event.get(token_BeamSpot);
 
       auto const& tracks_d_view = tracks_d.view();
-      std::cout << __LINE__ << std::endl;
-      Queue queue = event.queue();
+      auto queue = event.queue();
       int maxVertices = 10;
-      std::cout << __LINE__ << std::endl;
       const auto maxTracks = tracks_d_view.metadata().size();
       std::cout << "maxTracks = " << maxTracks << std::endl;
-      std::cout << __LINE__ << std::endl;
       //const uint32_t nTracks; // SEG FAULT HERE; cannot access nTracks since
       // it's data allocated in device
       // will need to use this in the kernel
       /*TkSoAHost host_tracks; 
       alpaka::memcpy(queue, host_tracks, tracks_d);*/
 
-      TracksHost<pixelTopology::Phase2> tracks_h(queue);  // in the host
+      auto tracks_h = cms::alpakatools::CopyToHost<TkSoADevice>::copyAsync(queue, tracks_d);
       alpaka::memcpy(queue, tracks_h.buffer(), tracks_d.buffer());
       alpaka::wait(queue);
-      std::cout << __LINE__ << std::endl;
       const uint32_t nTracks = tracks_h.view().nTracks();
 
       std::cout << "nTracks = " << nTracks << std::endl;
       //const uint32_t nTracks = maxTracks;  // placeholder
-      std::cout << __LINE__ << std::endl;
 
       ZVertexSoACollection vertices({{maxVertices, maxTracks}}, queue);  // this object is in the device
-      std::cout << __LINE__ << std::endl;
       auto data = vertices.view();
-      auto trkdata = vertices.view<reco::ZVertexTracksSoA>();  // access the data in the ZVertexTracksSoA Layout
-      auto vrtxdata = vertices.view<reco::ZVertexSoA>();       // access the data in the ZVertexSoA Layout
-      std::cout << __LINE__ << std::endl;
+      auto trkdata = vertices.view<::reco::ZVertexTracksSoA>();  // access the data in the ZVertexTracksSoA Layout
+      auto vrtxdata = vertices.view<::reco::ZVertexSoA>();       // access the data in the ZVertexSoA Layout
 
       std::vector<float> coords;
       std::vector<float> pts;
@@ -158,7 +151,7 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE {
         auto pt = (tracks_h.view()
                        .pt())[idx];  // instead of [idx] I was doing [idx + nTracks], but it never went into seg fault
         if (pt >= ptMin_) {
-          coords.push_back(reco::zip(tracks_h.view(), idx));
+          coords.push_back(::reco::zip(tracks_h.view(), idx));
           pts.push_back(pt);
           /*coords.at(it) = reco::zip(tracks_h.view(), idx);
           coords.at(it + nTracks) = pt;*/
@@ -175,125 +168,14 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE {
         coords.insert(coords.end(), pts.begin(), pts.end());
 
         std::cout << "coords.size() and results.size() = " << coords.size() << " " << results.size() << std::endl;
-        std::cout << __LINE__ << std::endl;
         clueVertexFinder::Producer clusterer(m_dc, m_rhoc, m_dm, m_pPBin, m_wtAvg);
         // ************TRYING TO LOAD DATA INTO WORKSPACE************
+		clusterer.makeClusters(queue, coords, results, trueTracks);
         std::cout << "Before calling makeAsync\n";
-        clusterer.makeAsync(queue, tracks_d_view, maxVertices, ptMin_);
+        // clusterer.makeAsync(queue, tracks_d_view, maxVertices, ptMin_);
         std::cout << "After calling makeAsync\n";
-        // THIS PART WORKS
-        /*clusterer.makeClusters(queue, coords, results, trueTracks);
-
-        std::cout << __LINE__ << std::endl;
-        auto myClusters = std::span<const int>{results.data(), trueTracks};
-        auto isSeed = std::span<const int>(results.data() + nTracks, trueTracks);
-
-        std::cout << "myClusters.size() = " << myClusters.size() << " and isSeed.size() = " << isSeed.size()
-                  << std::endl;
-
-        std::cout << __LINE__ << std::endl;
-        int nClusters = *(std::max_element(myClusters.begin(), myClusters.end())) + 1;
-        std::cout << "nClusters = " << nClusters << std::endl;
-        std::vector<int> clusterCount(nClusters);  // need this to calculate averages later
-        std::cout << __LINE__ << std::endl;*/
-      } else {
-        std::cout << "No tracks have pt greater than ptMin_ \n";
       }
-      /* ZVertexSoACollection is made of a ZVertexSoA and a ZVertexTracksSoA
-      // ZvertexSoA is made of:
-      //               SOA_COLUMN(float, zv),          // output z-posistion of found vertices
-                       SOA_COLUMN(float, wv),          // output weight (1/error^2) on the above
-                       SOA_COLUMN(float, chi2),        // vertices chi2
-                       SOA_COLUMN(float, ptv2),        // vertices pt^2
-                       SOA_COLUMN(uint16_t, sortInd),  // sorted index (by pt2)  ascending
-                       SOA_SCALAR(uint32_t, nvFinal))  // the number of vertices
-         and ZVertexTraksSoA is made of:
-         SOA_COLUMN(int16_t, idv),   // vertex index for each associated (original) track
-                                     // (-1 == not associate)
-         SOA_COLUMN(int32_t, ndof))  // vertices number of dof
-      */
-
-      // Let's start filling out "vertices" !!
-      // To fill out columns and scalars I have to use memcopys, cause I am on the host
-      /*auto zv_hbuff = cms::alpakatools::make_host_buffer<float[]>(queue, nClusters);
-      for(int i = 0; i < nClusters; ++i) {
-        zv_hbuff[i] = coords[i];
-      }*/
-
-      // I will need this to compute chi2 of each vertex
-      // std::for_each(myClusters.begin(), myClusters.end(), [&clusterCount](int idx) { clusterCount[idx]++; }); // SEG FAULT HERE
-
-      std::cout << __LINE__ << std::endl;
-
-      // vrtxdata.nvFinal() = nClusters; // SEG fault here, understandable: I'm trying to modify data in the device from the host
-
-      std::cout << __LINE__ << std::endl;
-      // Preparing to launch kernels
-
-      /*
-      for (auto i = 0u; i < nTracks; ++i) { 
-        if (isSeed[i]) {
-          vrtxdata[myClusters[i]].zv() = coords[i];
-          trkdata[i].ndof() = clusterCount[myClusters[i]] - 1;
-        }
-        trkdata[i].idv() = myClusters[i];
-        zAcc[myClusters[i]] += coords[i];
-      }
-
-      event.emplace(token_RecoVertex, std::move(vertices));
-      */
-
-      /* 
-
-      auto vertexes = std::make_unique<reco::VertexCollection>();
-      auto myClusters = algo.getClusters(h_points); // returns std::map<int, std::vector<int>> vertex to track ids map
-      auto seeds = h_points.isSeed(); // array of indexes of the seeds, there are myClusters.size() seeds
-	*/
-      // Need to put the clusters into "vertexes", use the seed of each cluster as the point, and then put the points of the
-      // cluster in the track of each Vector
-      //
-      /* if (bsHandle.isValid()) {
-        
-	const reco::BeamSpot& bs = *bsHandle;
-
-        for(auto s : seeds) {
-          double z = h_points.coords()[ s - 1 + 2 * n_points]; // coords() returns a single vector and it is ordered like
-	 						       // x0, x1, x2, ... , y0, y1, y2, ... , z0, z1, z2, ...	 
-      	  
-	  // not sure about this one, do we actually have the BeamSpot object?	  
-	  double x = bs.x0() + bs.dxdz() * (z - bs.z0());
-	  double y = bs.y0() + bs.dydz() * (z - bs.zo());
-
-	  int clusterIdx = h_points.clusterIndexes()[s];
-	  int clusterSize = myClusters[clusterIdx].size();
-
-	  // Hard coding error, chi2 and ndof
-	
-	  double avgPos = std::accumulate(myClusters[clusterIdx].brgin(), myClusters[clusterIdx].end(), 0.0,
-			  [&h_points.coords()](double acc, size_t i) { return acc + h_points.coords()[i + n_points * 2]; } );
-	  avgPos /= clusterSize;
-
-	  // Computing error in 1D(z): Semidispersione massima / sqrt(num of points in cluster)
-	  auto minmax = std::minmax_element(myClusters[clusterIdx].begin(), myClusters[clusterIdx].end(),
-			  [&h_points.coords()](size_t i, size_t j) { 
-			    return h_points.coords()[i + n_points * 2] < h_points.coords()[j + n_points * 2]; });
-	  double min = h_points.coords()[*minmax.first];
-	  double max = h_points.coords()[*minmax.second];
-
-	  double error = (max - min) / std::sqrt(clusterSize);
-
- 	  // Computing chi2 of z coordinate	
-	  
-
-	  double chi2 = std::accumulate(myClusters[clusterIdx].brgin(), myClusters[clusterIdx].end(), 0.0,
-		  	  [&h_points.coords()](double acc, size_t i) { 
-		    	    return acc + (h_points.coords()[i + n_points * 2] - avgPos)*(h_points.coords()[i + n_points * 2] - avgPos)/avgPos; } );
-	  // ***
-
-	  int ndof = clusterSize - 1;
-      }*/
-
-      // event.put(std::move(vertexes));
+	  event.put(std::move(vertices));
     }
 
   private:
