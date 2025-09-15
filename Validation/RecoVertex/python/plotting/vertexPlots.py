87ea41efb8f713adcdd8322a8516f21c64dca03c
Simone <simone.balducci00@gmail.com>
2025-09-15 10:58:18 +0200

    Add plots to vertexPlots

--- Validation/RecoVertex/python/plotting/vertexPlots.py
+++ Validation/RecoVertex/python/plotting/vertexPlots.py
@@ -426,10 +426,12 @@ _vertexFolders = [
     "DQMData/Vertexing/PrimaryVertex",
     "DQMData/Run 1/Vertexing/Run summary/PrimaryVertexV",
     "DQMData/Vertexing/PrimaryVertexV",
+    "DQMData/Run 1/HLT/Run summary/Vertexing/ValidationWRTsim/hltPhase2PixelVertices",
 ]
 _vertexDqmFolders = [
     "DQMData/Run 1/OfflinePV/Run summary/offlinePrimaryVertices",
     "DQMData/OffinePV/offlinePrimaryVertices",
+    "DQMData/Run 1/HLT/Run summary/Vertexing/ValidationWRTsim/hltPhase2PixelVertices",
 ]
 _v0Folders = [
     "DQMData/Run 1/Vertexing/Run summary/V0",
