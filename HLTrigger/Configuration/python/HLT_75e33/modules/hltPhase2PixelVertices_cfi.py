f5abbe38a4e7527406a9ae21e29edc85630102e6
Emanuele Coradin <emanuele.coradin@studenti.unipd.it>
2026-01-20 12:29:59 +0100

    Implemented PixelTrackSelectorDNN inference

--- HLTrigger/Configuration/python/HLT_75e33/modules/hltPhase2PixelVertices_cfi.py
+++ HLTrigger/Configuration/python/HLT_75e33/modules/hltPhase2PixelVertices_cfi.py
@@ -8,13 +8,7 @@ hltPhase2PixelVertices = cms.EDProducer("PixelVertexProducer",
         refToPSet_ = cms.string('pSetPvClusterComparerForIT')
     ),
     PtMin = cms.double(1.0),
-    # Even though pixel tracks with a highPurity ID, i.e. hltPhase2PixelTracks,
-    # are used in other tracking modules, the pixel tracks without an ID,
-    # i.e. hltPhase2PixelTracksCAExtension, are used here.
-    # This avoids a circular dependency, as the highPurity ID requires a vertex,
-    # while also providing satisfactory physics performance.
-    # To be improved with a DNN-based highPurity ID that does not depend on vertices.
-    TrackCollection = cms.InputTag("hltPhase2PixelTracksCAExtension"),
+    TrackCollection = cms.InputTag("hltPhase2PixelTracks"),
     UseError = cms.bool(True),
     Verbosity = cms.int32(0),
     WtAverage = cms.bool(True),
